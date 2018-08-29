15be4a9e8c8ea19444fdcef859327e25afc1e482
zhongying1995 <1424249327@qq.com>
2018-08-29 22:17:08 +0800

    增加ydwe打开地图的方式

--- tools/YDWE编辑地图.lua
+++ tools/YDWE编辑地图.lua
@@ -1,4 +1,5 @@
 require 'filesystem'
+local registry = require 'registry'
 local ydwe = require 'tools.ydwe'
 local process = require 'process'
 if not ydwe then
@@ -10,7 +11,7 @@ if not fs.exists(root / 'JustForLiving.w3x') then
     print('地图不存在', root / 'JustForLiving.w3x')
     return
 end
-local command = (registry.current_user() / [[SOFTWARE\Classes\YDWEMap\shell\open\command]])['']
+local command = (registry.open [[HKEY_CURRENT_USER\SOFTWARE\Classes\YDWEMap\shell\open\command]])['']
 command = command:gsub("%%1", (root / 'JustForLiving.w3x'):string())
 print(command)
 local p = process()
