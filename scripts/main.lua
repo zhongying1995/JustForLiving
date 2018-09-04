37ce699c7275c3bcd382c29b8ba358ea1b547874
zhongying1995 <1424249327@qq.com>
2018-09-04 21:56:59 +0800

    新增一个马甲单位，并初次解包

--- scripts/main.lua
+++ scripts/main.lua
@@ -4,11 +4,13 @@ function print(...)
 	std_print(('[%.3f]'):format(os.clock()), ...)
 end
 
+
 local function main()
 	print 'hello loli!'
 	print ('package.path = ', package.path)
 	print('hello 一中11231233')
 	require("War3Library._init")
+	
 end
 
 main()
