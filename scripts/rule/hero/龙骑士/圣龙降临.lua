ae67d8b513a2ba988fec83ff8e628a19bab8cb1e
zhongying1995 <1424249327@qq.com>
2018-10-10 22:24:56 +0800

    龙骑士技能 - 圣龙降临

--- scripts/rule/hero/龙骑士/圣龙降临.lua
+++ scripts/rule/hero/龙骑士/圣龙降临.lua
@@ -1,3 +1,43 @@
 local mt = ac.skill['圣龙降临']{
-    
+    war3_id = 'A303',
+    life = 1000,
+    life_pers = {10, 15, 20, 25, 30},
+    defence = 10,
+    times = {15, 20, 25, 30, 35},
+    tranform_id = 'H202',
+    tranform_effect_model = [[Abilities\Spells\Orc\FeralSpirit\feralspirittarget.mdl]],
 }
+
+function mt:on_effect()
+    local unit = self.owner
+    
+    local level = self:get_level()
+    local time = self.times[level]
+    local id = unit:get_id()
+    if id ~= self.tranform_id then
+        self:set('id', id)
+    end
+    
+    local tranform_timer = self:get('tranform_timer')
+    if tranform_timer then
+        tranform_timer:remove()
+    else
+        unit:transform(self.tranform_id)
+        local life_per = self.life_pers[level]
+        local life = self.life + unit:get_max_life() * life_per / 100
+        self:set('life', life)
+        unit:add_max_life(life)
+        unit:add_defence(self.defence)
+    end
+    unit:add_effect(self.tranform_effect_model, 'chest'):remove()
+
+    self.tranform_timer = ac.wait(time * 1000, function()
+        local id = self:get('id')
+        local life = self:get('life')
+        unit:transform(id)
+        unit:add_max_life(-life)
+        unit:add_defence(-self.defence)
+        self:set('tranform_timer', nil)
+    end)
+    self:set('tranform_timer', self.tranform_timer)
+end
