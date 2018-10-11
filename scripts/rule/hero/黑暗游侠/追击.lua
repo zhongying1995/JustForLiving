9a7ef3e067051b5a362c9f960c3ee56b086244ad
zhongying1995 <1424249327@qq.com>
2018-10-11 22:51:26 +0800

    黑暗游侠-追击

--- scripts/rule/hero/黑暗游侠/追击.lua
+++ scripts/rule/hero/黑暗游侠/追击.lua
@@ -1,3 +1,44 @@
 local mt = ac.skill['追击']{
     war3_id = 'A305',
+    effect_model = [[Abilities\Spells\Other\BlackArrow\BlackArrowMissile.mdl]],
+    rates = {20, 25, 30, 35, 40},
 }
+
+function mt:on_add()
+    local unit = self.owner
+    self.trg = unit:event '单位-即将造成伤害'(function(trg, damage)
+        if not damage:is_attack() then
+            return
+        end
+        local level = self:get_level()
+        local rate = self.rates[level]
+        if rate < math.random(1, 100) then
+            return
+        end
+        local target = damage.target
+        local high = unit:get_slk('launchZ', 60) + unit:get_high()
+        local mvr = ac.mover.target{
+            start = unit,
+            model = self.effect_model,
+            target = target,
+            high = high,
+            speed = 900,
+            skill = self,
+        }
+        if mvr then
+            function mvr:on_finish()
+                unit:damage{
+                    target = self.target,
+                    damage = unit:get_agi(true),
+                    damage_type = '魔法',
+                }
+            end
+        end
+    end)
+end
+
+function mt:on_remove()
+    if self.trg then
+        self.trg:remove()
+    end
+end
