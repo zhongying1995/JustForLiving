f6f582478f34a0b0da679deae660bc7be6af0bef
zhongying1995 <1424249327@qq.com>
2018-10-15 23:28:22 +0800

    龙骑士-钢铁之躯，重写

--- scripts/rule/hero/龙骑士/钢铁之躯.lua
+++ scripts/rule/hero/龙骑士/钢铁之躯.lua
@@ -2,27 +2,46 @@ local mt = ac.skill['钢铁之躯']{
     war3_id = 'A302',
     recovery = {3, 5, 10, 15, 20},
     layer = {5, 10, 15, 20, 25},
-    time = {10, 12, 14, 16, 20}
+    time = {10, 12, 14, 16, 20},
+    passive = true,
+}
+
+function mt:on_enable()
+    local unit = self.owner
+    unit:add_buff('龙骑士-钢铁之躯'){
+        skill = self,
+    }
+end
+
+function mt:on_disable()
+    local unit = self.owner
+    unit:remove_buff('龙骑士-钢铁之躯')
+end
+
+function mt:on_upgrade()
+    if self:is_enable() then
+        local unit = self.owner
+        unit:remove_buff('龙骑士-钢铁之躯')
+        unit:add_buff('龙骑士-钢铁之躯'){
+            skill = self,
+        }
+    end
+end
+
+local mt = ac.buff['龙骑士-钢铁之躯']{
+
 }
 
 function mt:on_add()
-    local unit = self.owner
-    self.trg = unit:event '单位-即将受到伤害'(function()
-        local level = self:get_level()
-        local max_layer = self.layer
-        if unit._gangTieZhiQu_buff_lv == nil then
-            unit._gangTieZhiQu_buff_lv = 0
-        end
-        local layer = unit._gangTieZhiQu_buff_lv
-        if layer < max_layer then
-            unit._gangTieZhiQu_buff_lv = unit._gangTieZhiQu_buff_lv + 1
-            local time = self.time
-            local recovery = self.recovery
-            unit:add_life_recovery(recovery)
-            ac.wait(time*1000, function()
-                unit:add_life_recovery(-recovery)
-            end)
-        end
+    local unit = self.target
+    local skill = self.skill
+    self.trg = unit:event '单位-即将受到伤害'(function(trg, damage)
+        unit:add_buff('龙骑士-钢铁之躯-恢复'){
+            skill = skill,
+            cover_max = skill.layer,
+            recovery = skill.recovery,
+            time = skill.time,
+        }
     end)
 end
 
@@ -30,4 +49,21 @@ function mt:on_remove()
     if self.trg then
         self.trg:remove()
     end
+end
+
+
+local mt = ac.buff['龙骑士-钢铁之躯-恢复']{
+    cover_type = 1,
+}
+
+function mt:on_add()
+    local unit = self.target
+    local skill = self.skill
+    unit:add_life_recovery(self.recovery)
+end
+
+function mt:on_remove()
+    local unit = self.target
+    local skill = self.skill
+    unit:add_life_recovery(-self.recovery)
 end
