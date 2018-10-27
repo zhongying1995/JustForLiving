ca7ec0f98735cfdc6491c63d39b48043ac342b20
zhongying1995 <1424249327@qq.com>
2018-10-27 23:55:41 +0800

    选择模式，用新的方式去写

--- scripts/rule/model/game_degree.lua
+++ scripts/rule/model/game_degree.lua
@@ -4,58 +4,114 @@ setmetatable(Game_degree, Game_degree)
 local mt = {}
 Game_degree.__index = mt
 
+setmetatable(mt, {
+    __newindex = function ( t, k, v )
+        if type(v) == 'table' and v.action then
+            setmetatable(v, v)
+            v.__index = t
+        end
+        rawset(t, k, v)
+    end
+})
+
+--设置普通回合最大波数
 function mt:set_round_number(n)
-    --设置普通回合最大波数
     local round = require 'rule.round.normal_round'
     round:set_max_round_number(n)
+end
+
+--获取进攻怪的玩家
+function mt:get_invade_creep_player()
+    if not self.invade_creep_player then
+        self.invade_creep_player = ac.player.force[1][1]:get_invade_creep_player()
+    end
+    return self.invade_creep_player
 end
 
 function mt:show_degree_msg(msg, time)
     ac.player.self:send_msg(msg, time)
 end
 
+--增加进攻野怪的难度攻击力
+--  百分比
+function mt:add_invade_creep_attack(atk_rate)
+    local player = self:get_invade_creep_player()
+    self.invade_creep_attack_trg = player:event '单位-创建'(function(trg, unit)
+        local atk = unit:get_attack() * atk_rate / 100
+        unit:add_add_attack(atk)
+    end)
+end
 
-mt['小白过家家级'] = function(self, player)
-    print(player:tostring(), '点击了 小白过家家级 难度')
-    local msg = [[
+--增加进攻野怪的防御
+--  百分比
+function mt:add_invade_creep_defence(def_rate)
+    local player = self:get_invade_creep_player()
+    self.invade_creep_defence_trg = player:event '单位-创建'(function(trg, unit)
+        local def = unit:get_defence() * def_rate / 100
+        unit:add_add_defence(def)
+    end)
+end
+
+
+mt['小白过家家级'] = {
+    msg = [[
         |cffff0000小白过家家难度|r：
         地图敌对怪物不会变异
         生存30波后会迎来最终回合
         无特殊怪物进攻
-    ]]
-    self:show_degree_msg(msg, 20)
+    ]],
+    action = function(self, player)
+        print(player:tostring(), '点击了 小白过家家级 难度')
+        self:show_degree_msg(self.msg, 20)
 
-    self:set_round_number(30)
-end
+        self:set_round_number(30)
+    end
 
-mt['老鸟各自飞级'] = function(self, player)
-    print(player:tostring(), '点击了 老鸟各自飞级 难度')
-    local msg = [[
+}
+
+mt['老鸟各自飞级'] = {
+    msg = [[
         |cffff0000老鸟各自飞难度|r：
         可以选择 生存 | 防守 | 生存+防守模式
         地图敌对怪物会变异
         36波后，迎来最终回合
         小概率触发特殊怪物进攻
-    ]]
-    self:show_degree_msg(msg, 20)
+    ]],
+    action = function(self, player)
+        print(player:tostring(), '点击了 老鸟各自飞级 难度')
+        self:show_degree_msg(self.msg, 20)
 
-    self:set_round_number(30)
-end
+        self:set_round_number(30)
+        
+        --进攻怪玩家
+        local p = player:get_invade_creep_player()
+        --设置war3科技等级
+        p:set_research_level('R101', 1)
 
-mt['老鸟劝退级'] = function(self, player)
-    print(player:tostring(), '点击了 老鸟劝退级 难度')
-    local msg = [[
-        |cffff0000老鸟劝退飞难度|r：
-        可以选择 生存 | 防守 | 生存+防守模式
-        地图敌对怪物会大幅变异
-        36波后，迎来最终回合
-        概率触发特殊怪物进攻
-        触发游戏彩蛋
-    ]]
-    self:show_degree_msg(msg, 30)
+    end
+}
 
-    self:set_round_number(30)
-end
+mt['老鸟劝退级'] = {
+    msg = [[
+            |cffff0000老鸟劝退飞难度|r：
+            可以选择 生存 | 防守 | 生存+防守模式
+            地图敌对怪物会大幅变异
+            36波后，迎来最终回合
+            概率触发特殊怪物进攻
+            触发游戏彩蛋
+    ]],
+    action = function(self, player)
+        print(player:tostring(), '点击了 老鸟劝退级 难度')
+        self:show_degree_msg(self.msg, 30)
+
+        self:set_round_number(30)
+
+        --进攻怪玩家
+        local p = player:get_invade_creep_player()
+        --设置war3科技等级
+        p:set_research_level('R102', 1)
+    end
+}
 
 
 return Game_degree
--- scripts/rule/model/game_model.lua
+++ scripts/rule/model/game_model.lua
@@ -4,33 +4,48 @@ setmetatable(Game_model, Game_model)
 local mt = {}
 Game_model.__index = mt
 
+setmetatable(mt, {
+    __newindex = function ( t, k, v )
+        if type(v) == 'table' and v.action then
+            setmetatable(v, v)
+            v.__index = t
+        end
+        rawset(t, k, v)
+    end
+})
 
-mt['生存模式'] = function(self, player)
-    print(player:tostring(), '点击了 生存模式')
-    local msg = [[
+mt['生存模式'] = {
+    msg = [[
         |cffff0000生存模式|r：
         回合内死亡不会复活，直到新的回合开始
-    ]]
-    ac.player.self:send_msg(msg, 20)
-end
+    ]],
+    action = function(self, player)
+        print(player:tostring(), '点击了 生存模式')
+        ac.player.self:send_msg(self.msg, 20)
+    end
+}
 
-mt['防守模式'] = function(self, player)
-    print(player:tostring(), '点击了 防守模式')
-    local msg = [[
+mt['防守模式'] = {
+    msg = [[
         |cffff0000防守模式|r：
         玩家需要守护中心区域的基地
         进攻怪会直接对建筑发起进攻
-    ]]
-    ac.player.self:send_msg(msg, 20)
-end
+    ]],
+    action = function(self, player)
+        print(player:tostring(), '点击了 防守模式')
+        ac.player.self:send_msg(self.msg, 20)
+    end
+}
 
-mt['生存+防守模式'] = function(self, player)
-    print(player:tostring(), '点击了 生存+防守模式')
-    local msg = [[
+mt['生存+防守模式'] = {
+    msg = [[
         |cffff0000生存+防守模式|r：
         年轻？？？
-    ]]
-    ac.player.self:send_msg(msg, 30)
+    ]],
+    action = function(self, player)
+        print(player:tostring(), '点击了 生存+防守模式')
+        ac.player.self:send_msg(self.msg, 30)
 end
+}
 
 return Game_model
