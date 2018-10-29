b1e8b05ce66cf9e6cbfa38640823cceeebd8c3cc
zhongying1995 <1424249327@qq.com>
2018-10-29 21:10:07 +0800

    回合增加默认的阶段

--- scripts/rule/round/round.lua
+++ scripts/rule/round/round.lua
@@ -6,5 +6,27 @@ Round.__index = mt
 
 mt.type = '回合'
 
+--回合状态，四种状态，创建、准备、开始、结束
+mt.state = nil
+
+function mt:init()
+
+end
+
+function mt:create()
+    self.state = '创建'
+end
+
+function mt:prepare()
+    self.state = '准备'
+end
+
+function mt:start()
+    self.state = '开始'
+end
+
+function mt:stop()
+    self.state = '结束'
+end
 
 return Round
