582cf72e494541222bd63a1a17be63af1314a64d
zhongying1995 <1424249327@qq.com>
2018-11-03 22:47:23 +0800

    物品-神石

--- scripts/item/专属/神石.lua
+++ scripts/item/专属/神石.lua
@@ -1,8 +1,20 @@
 local mt = ac.item['神石']{
-    war3_id = 'I100'
+    war3_id = 'I100',
+    --最大的可用次数
+    max_stack = 100,
 }
 
 function mt:on_add()
     local unit = self.owner
-    print(unit:get_name(), self.name)
+    self.kill_unit_trg = unit:event '单位-杀死单位'(function(trg, killer, killed)
+        if self:get_stack() < self.max_stack then
+            self:add_stack()
+        end
+    end)
+end
+
+function mt:on_drop()
+    if self.kill_unit_trg then
+        self.kill_unit_trg:remove()
+    end
 end
