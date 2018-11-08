8c4a2379fc0db1febb70e9e894fe0cf564556e65
zhongying1995 <1424249327@qq.com>
2018-11-08 23:03:36 +0800

    增加合成白银级

--- scripts/rule/shop/item_shop/_init.lua
+++ scripts/rule/shop/item_shop/_init.lua
@@ -1,2 +1,3 @@
 require 'rule.shop.item_shop.合成废铁级'
 require 'rule.shop.item_shop.合成青铜级'
+require 'rule.shop.item_shop.合成白银级'
---
+++ scripts/rule/shop/item_shop/合成白银级.lua
@@ -0,0 +1,80 @@
+local Unit = Router.unit
+
+local mt = ac.unit_button['合成白银级']{
+    war3_id = 'h502',
+    effect_model = [[Abilities\Spells\Items\AIsm\AIsmTarget.mdl]],
+}
+
+function mt:on_click()
+    local unit = self.clicker
+    if not unit:has_item('纯银') then
+        self:failed('你没有纯银！')
+        return
+    end
+
+    if unit:has_item('黄铜魂珠') then
+        unit:remove_item('黄铜魂珠')
+        unit:add_item('纯银魂珠')
+        self:succeed()
+        return
+    end
+
+    if unit:has_item('黄铜项链') then
+        unit:remove_item('黄铜项链')
+        unit:add_item('纯银项链')
+        self:succeed()
+        return
+    end
+
+    if unit:has_item('黄铜剑') then
+        unit:remove_item('黄铜剑')
+        unit:add_item('纯银剑')
+        self:succeed()
+        return
+    end
+
+    if unit:has_item('黄铜弓') then
+        unit:remove_item('黄铜弓')
+        unit:add_item('纯银弓')
+        self:succeed()
+        return
+    end
+
+    if unit:has_item('黄铜杖') then
+        unit:remove_item('黄铜杖')
+        unit:add_item('纯银杖')
+        self:succeed()
+        return
+    end
+
+    if unit:has_item('黄铜铃') then
+        unit:remove_item('黄铜铃')
+        unit:add_item('纯银铃')
+        self:succeed()
+        return
+    end
+
+    if unit:has_item('黄铜镰') then
+        unit:remove_item('黄铜镰')
+        unit:add_item('纯银镰')
+        self:succeed()
+        return
+    end
+
+end
+
+function mt:failed( msg )
+    local unit = self.clicker
+    local p = unit:get_owner()
+    p:send_msg(msg, 3)
+    local gold = Unit.get_slk_by_id(self.war3_id, 'goldcost', 0)
+    local lumber = Unit.get_slk_by_id(self.war3_id, 'lumbercost', 0)
+    p:add_gold(gold)
+    p:add_lumber(lumber)
+end
+
+function mt:succeed()
+    local unit = self.clicker
+    unit:add_effect(self.effect_model):remove()
+    unit:remove_item('纯银')
+end
