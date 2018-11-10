4b3c9b465b95971297f8adeb9c416ba01403dcd1
zhongying1995 <1424249327@qq.com>
2018-11-10 20:43:58 +0800

    为单位增加物品的测试指令，运行创建多个物品

--- scripts/test/command.lua
+++ scripts/test/command.lua
@@ -2,12 +2,15 @@ local Debug_command = Router.debug_command
 local mt = Debug_command.__index
 
 mt['item'] = {
-    action = function (player, name)
+    action = function (player, name, n)
         local u = player:get_selecting_unit()
         if u then
-            local it = u:add_item(name)
-            if not it then
-                player:send_msg('请检查物品名称是否正确：', name)
+            local n = n or 1
+            for i = 1, n do
+                local it = u:add_item(name)
+                if not it then
+                    player:send_msg('请检查物品名称是否正确：', name)
+                end
             end
         else
             player:send_msg('请选择您的英雄！')
