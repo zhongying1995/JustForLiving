ddb9e943779244d48952bd56f5eafa162d4d22f5
zhongying1995 <1424249327@qq.com>
2018-11-15 23:16:58 +0800

    base增加unit模块，unit模块封装抽点方法

---
+++ scripts/base/unit.lua
@@ -0,0 +1,12 @@
+
+local Unit = Router.unit
+
+local mt = Unit.__index
+
+--抽点
+--  期待的概率
+--  [最小的概率]
+--  [最大的概率]
+function mt:roll_fortune( rate, min, max )
+    return self:get_owner(rate, min, max)
+end
