a62c0e0ee4650c01de27ac726b9e530a59568b46
zhongying1995 <1424249327@qq.com>
2018-11-17 13:34:39 +0800

    三个光环名字没改

--- scripts/rule/shop/skill_shop/aura_skill/霜冻光环.lua
+++ scripts/rule/shop/skill_shop/aura_skill/霜冻光环.lua
@@ -1,6 +1,6 @@
 local Skill_shop = require 'rule.shop.skill_shop.skill_shop'
 
-local skill_name = '吸血光环-商店学习的'
+local skill_name = '霜冻光环-商店学习的'
 local skill_mt = ac.skill[skill_name]{
     war3_id = 'A20D'
 }
