2411406460c10000a2ad12d549fa40c2d200d63e
zhongying1995 <1424249327@qq.com>
2018-11-17 11:00:02 +0800

    底层的create_callback错了

--- scripts/unit/invade/normal_registries.lua
+++ scripts/unit/invade/normal_registries.lua
@@ -7,302 +7,218 @@ ac.unit['主城']{
 --注册boss
 ac.unit['Boss进攻怪-第1波']{
     war3_id = 'O101',
-    create_callback = function(self)
-        self.is_invade_creep = true
-        self.is_invade_boss = true
-    end
+    is_invade_creep = true,
+    is_invade_boss = true,
 }
 
 ac.unit['Boss进攻怪-第2波']{
     war3_id = 'O102',
-    create_callback = function(self)
-        self.is_invade_creep = true
-        self.is_invade_boss = true
-    end
+    is_invade_creep = true,
+    is_invade_boss = true,
 }
 
 ac.unit['Boss进攻怪-第3波']{
     war3_id = 'O103',
-    create_callback = function(self)
-        self.is_invade_creep = true
-        self.is_invade_boss = true
-    end
+    is_invade_creep = true,
+    is_invade_boss = true,
 }
 
 ac.unit['Boss进攻怪-第4波']{
     war3_id = 'O104',
-    create_callback = function(self)
-        self.is_invade_creep = true
-        self.is_invade_boss = true
-    end
+    is_invade_creep = true,
+    is_invade_boss = true,
 }
 
 ac.unit['Boss进攻怪-第5波']{
     war3_id = 'O105',
-    create_callback = function(self)
-        self.is_invade_creep = true
-        self.is_invade_boss = true
-    end
+    is_invade_creep = true,
+    is_invade_boss = true,
 }
 
 ac.unit['Boss进攻怪-第6波']{
     war3_id = 'O100',
-    create_callback = function(self)
-        self.is_invade_creep = true
-        self.is_invade_boss = true
-    end
+    is_invade_creep = true,
+    is_invade_boss = true,
 }
 
 
 --注册普通怪物
 ac.unit['普通进攻怪-第1波']{
     war3_id = 'h100',
-    create_callback = function(self)
-        self.is_invade_creep = true
-    end
+    is_invade_creep = true,
 }
 
 ac.unit['普通进攻怪-第2波']{
     war3_id = 'h101',
-    create_callback = function(self)
-        self.is_invade_creep = true
-    end
+    is_invade_creep = true,
 }
 
 ac.unit['普通进攻怪-第3波']{
     war3_id = 'h102',
-    create_callback = function(self)
-        self.is_invade_creep = true
-    end
+    is_invade_creep = true,
 }
 
 ac.unit['普通进攻怪-第4波']{
     war3_id = 'h103',
-    create_callback = function(self)
-        self.is_invade_creep = true
-    end
+    is_invade_creep = true,
 }
 
 ac.unit['普通进攻怪-第5波']{
     war3_id = 'h104',
-    create_callback = function(self)
-        self.is_invade_creep = true
-    end
+    is_invade_creep = true,
 }
 
 ac.unit['普通进攻怪-第6波']{
     war3_id = 'h105',
-    create_callback = function(self)
-        self.is_invade_creep = true
-    end
+    is_invade_creep = true,
 }
 
 ac.unit['普通进攻怪-第7波']{
     war3_id = 'h106',
-    create_callback = function(self)
-        self.is_invade_creep = true
-    end
+    is_invade_creep = true,
 }
 
 ac.unit['普通进攻怪-第8波']{
     war3_id = 'h107',
-    create_callback = function(self)
-        self.is_invade_creep = true
-    end
+    is_invade_creep = true,
 }
 
 ac.unit['普通进攻怪-第9波']{
     war3_id = 'h108',
-    create_callback = function(self)
-        self.is_invade_creep = true
-    end
+    is_invade_creep = true,
 }
 
 ac.unit['普通进攻怪-第10波']{
     war3_id = 'h109',
-    create_callback = function(self)
-        self.is_invade_creep = true
-    end
+    is_invade_creep = true,
 }
 
 ac.unit['普通进攻怪-第11波']{
     war3_id = 'h10A',
-    create_callback = function(self)
-        self.is_invade_creep = true
-    end
+    is_invade_creep = true,
 }
 
 ac.unit['普通进攻怪-第12波']{
     war3_id = 'h10B',
-    create_callback = function(self)
-        self.is_invade_creep = true
-    end
+    is_invade_creep = true,
 }
 
 ac.unit['普通进攻怪-第13波']{
     war3_id = 'h10C',
-    create_callback = function(self)
-        self.is_invade_creep = true
-    end
+    is_invade_creep = true,
 }
 
 ac.unit['普通进攻怪-第14波']{
     war3_id = 'h10D',
-    create_callback = function(self)
-        self.is_invade_creep = true
-    end
+    is_invade_creep = true,
 }
 
 ac.unit['普通进攻怪-第15波']{
     war3_id = 'h10E',
-    create_callback = function(self)
-        self.is_invade_creep = true
-    end
+    is_invade_creep = true,
 }
 
 ac.unit['普通进攻怪-第16波']{
     war3_id = 'h10F',
-    create_callback = function(self)
-        self.is_invade_creep = true
-    end
+    is_invade_creep = true,
 }
 
 ac.unit['普通进攻怪-第17波']{
     war3_id = 'h10G',
-    create_callback = function(self)
-        self.is_invade_creep = true
-    end
+    is_invade_creep = true,
 }
 
 ac.unit['普通进攻怪-第18波']{
     war3_id = 'h10H',
-    create_callback = function(self)
-        self.is_invade_creep = true
-    end
+    is_invade_creep = true,
 }
 
 ac.unit['普通进攻怪-第19波']{
     war3_id = 'h10I',
-    create_callback = function(self)
-        self.is_invade_creep = true
-    end
+    is_invade_creep = true,
 }
 
 ac.unit['普通进攻怪-第20波']{
     war3_id = 'h10J',
-    create_callback = function(self)
-        self.is_invade_creep = true
-    end
+    is_invade_creep = true,
 }
 
 ac.unit['普通进攻怪-第21波']{
     war3_id = 'h10K',
-    create_callback = function(self)
-        self.is_invade_creep = true
-    end
+    is_invade_creep = true,
 }
 
 ac.unit['普通进攻怪-第22波']{
     war3_id = 'h10L',
-    create_callback = function(self)
-        self.is_invade_creep = true
-    end
+    is_invade_creep = true,
 }
 
 ac.unit['普通进攻怪-第23波']{
     war3_id = 'h10M',
-    create_callback = function(self)
-        self.is_invade_creep = true
-    end
+    is_invade_creep = true,
 }
 
 ac.unit['普通进攻怪-第24波']{
     war3_id = 'h10N',
-    create_callback = function(self)
-        self.is_invade_creep = true
-    end
+    is_invade_creep = true,
 }
 
 ac.unit['普通进攻怪-第25波']{
     war3_id = 'h10O',
-    create_callback = function(self)
-        self.is_invade_creep = true
-    end
+    is_invade_creep = true,
 }
 
 ac.unit['普通进攻怪-第26波']{
     war3_id = 'h10P',
-    create_callback = function(self)
-        self.is_invade_creep = true
-    end
+    is_invade_creep = true,
 }
 
 ac.unit['普通进攻怪-第27波']{
     war3_id = 'h10Q',
-    create_callback = function(self)
-        self.is_invade_creep = true
-    end
+    is_invade_creep = true,
 }
 
 ac.unit['普通进攻怪-第28波']{
     war3_id = 'h10R',
-    create_callback = function(self)
-        self.is_invade_creep = true
-    end
+    is_invade_creep = true,
 }
 
 ac.unit['普通进攻怪-第29波']{
     war3_id = 'h10S',
-    create_callback = function(self)
-        self.is_invade_creep = true
-    end
+    is_invade_creep = true,
 }
 
 ac.unit['普通进攻怪-第30波']{
     war3_id = 'h10T',
-    create_callback = function(self)
-        self.is_invade_creep = true
-    end
+    is_invade_creep = true,
 }
 
 ac.unit['普通进攻怪-第31波']{
     war3_id = 'h10U',
-    create_callback = function(self)
-        self.is_invade_creep = true
-    end
+    is_invade_creep = true,
 }
 
 ac.unit['普通进攻怪-第32波']{
     war3_id = 'h10V',
-    create_callback = function(self)
-        self.is_invade_creep = true
-    end
+    is_invade_creep = true,
 }
 
 ac.unit['普通进攻怪-第33波']{
     war3_id = 'h10W',
-    create_callback = function(self)
-        self.is_invade_creep = true
-    end
+    is_invade_creep = true,
 }
 
 ac.unit['普通进攻怪-第34波']{
     war3_id = 'h10X',
-    create_callback = function(self)
-        self.is_invade_creep = true
-    end
+    is_invade_creep = true,
 }
 
 ac.unit['普通进攻怪-第35波']{
     war3_id = 'h10Y',
-    create_callback = function(self)
-        self.is_invade_creep = true
-    end
+    is_invade_creep = true,
 }
 
 ac.unit['普通进攻怪-第36波']{
     war3_id = 'h10Z',
-    create_callback = function(self)
-        self.is_invade_creep = true
-    end
+    is_invade_creep = true,
 }
