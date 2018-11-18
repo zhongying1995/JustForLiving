69421f673fa69f055806e0b51d2dcbee9d213723
zhongying1995 <1424249327@qq.com>
2018-11-18 23:41:35 +0800

    技能，特效药

--- scripts/rule/shop/skill_shop/active_skill/特效药.lua
+++ scripts/rule/shop/skill_shop/active_skill/特效药.lua
@@ -2,11 +2,22 @@ local Skill_shop = require 'rule.shop.skill_shop.skill_shop'
 
 local skill_name = '特效药'
 local mt = ac.skill[skill_name]{
-    war3_id = 'A209'
+    war3_id = 'A209',
+    base_recovery = {30, 50, 80, 100, 150},
+    str_rate = {0.05, 0.05, 0.05, 0.05, 0.05},
+    duration = 10,
 }
 
 function mt:on_effect()
-    
+    local unit = self.owner
+    local target = self.target
+    local heal = unit:get_str(true) * self.str_rate + self.base_recovery
+    unit:heal{
+        target = target,
+        heal = heal*self.duration,
+        skill = self,
+        duration = self.duration,
+    }
 end
 
 
