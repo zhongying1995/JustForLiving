6b3924ff5c9523bf9dc894af0216546dce0d98d2
zhongying1995 <1424249327@qq.com>
2018-11-19 00:48:34 +0800

    技能-腐臭蜂群

--- scripts/rule/shop/skill_shop/active_skill/腐臭蜂群.lua
+++ scripts/rule/shop/skill_shop/active_skill/腐臭蜂群.lua
@@ -2,11 +2,42 @@ local Skill_shop = require 'rule.shop.skill_shop.skill_shop'
 
 local skill_name = '腐臭蜂群'
 local mt = ac.skill[skill_name]{
-    war3_id = 'A206'
+    war3_id = 'A206',
+    base_damage = {100, 200, 300, 400, 500},
+    str_rate = {0.2, 0.25, 0.3, 0.35, 0.4},
+    effect = [[Abilities\Spells\Undead\CarrionSwarm\CarrionSwarmMissile.mdl]],
+    hit_area = 225,
+    range = {700, 800, 900, 1050, 1300},
+    speed = 1000,
 }
 
 function mt:on_effect()
-    
+    local unit = self.owner
+    local target = self.target
+    local point = unit:get_point()
+    local ang = point / target
+    local dmg = self.base_damage + unit:get_str(true) * self.str_rate
+    local mvr = ac.mover.line{
+        source = unit,
+        model = self.effect,
+        start = point,
+        distance = self.range,
+        speed = self.speed,
+        angle = ang,
+        hit_area = self.hit_area,
+        skill = self,
+    }
+    if mvr then
+        function mvr:on_hit( u )
+            if u:is_enemy(unit) then
+                unit:damage{
+                    target = u,
+                    damage = dmg,
+                    damage_type = '魔法',
+                }
+            end
+        end
+    end
 end
 
 
