538cbedfa98ccc1a1e61808489fe823494bf774f
zhongying1995 <1424249327@qq.com>
2018-11-21 22:10:22 +0800

    增加-minion测试指令

--- scripts/test/command.lua
+++ scripts/test/command.lua
@@ -1,3 +1,5 @@
+local Map_rects = require 'base.rects'
+
 local Debug_command = Router.debug_command
 local mt = Debug_command.__index
 
@@ -14,6 +16,18 @@ mt['item'] = {
             end
         else
             player:send_msg('请选择您的英雄！')
+        end
+    end
+}
+
+mt['minion'] = {
+    action = function ( player, index, number )
+        local index = index or 1
+        local number = number or 1
+        local name = '普通进攻怪-第' .. index .. '波'
+        local point = Map_rects['进攻怪物出生点-生存'][1]:get_point()
+        for i = 1, number do
+            ac.player[12]:create_unit(name, point)
         end
     end
 }
