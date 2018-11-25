94726b79aa15c2226b943ed2ef98842c5d0f5dd3
zhongying1995 <1424249327@qq.com>
2018-11-26 00:36:27 +0800

    第一版回合调度核心（未校验的）

--- scripts/rule/round/round_core.lua
+++ scripts/rule/round/round_core.lua
@@ -1,7 +1,5 @@
--- local Normal_round = require 'rule.round.normal_round'
--- local Boss_round = require 'rule.round.boss_round'
--- local Special_round = require 'rule.round.special_round'
--- local Final_round = require 'rule.round.final_round'
+local Game_degree = require 'rule.model.game_degree'
+local Game_model = require 'rule.model.game_model'
 local Fogmodifier = Router.fogmodifier
 local Rects = require 'base.rects'
 
@@ -12,8 +10,8 @@ setmetatable(Round_core, Round_core)
 local mt = {}
 Round_core.__index = mt
 
---每间隔 6 回合，出现一次boss
-local normal_round_switch_interval = 6
+--回合一共运行了几次
+mt.round_run_times = 0
 
 --初始化-切换(调度算法)-创建-结束-切换
 
@@ -21,46 +19,67 @@ local normal_round_switch_interval = 6
 --调度算法不应该被外界调用
 --  回合调度self
 --  上一个结束的回合
-local function switch_round(self, finish_round)
+function mt:switch_round(self, finish_round)
 
     local type = finish_round and finish_round.type
 
     if type == '普通回合' then
         local round_index = finish_round.index
-        print('普通回合结束', round_index)
-        if round_index % normal_round_switch_interval == 0 then
-            self.current_round = Boss_round
+        if round_index % self.normal_round_switch_interval == 0 then
+            self.next_round = self.boss_round
         else
-            self.current_round = Normal_round
+            self.next_round = self.normal_round
         end
     elseif type == 'boss回合' then
-        print('boss回合')
-        local n = Normal_round:get_round_number()
-        if n == Normal_round:get_max_round_number() then
-            self.current_round = Final_round
+        local n = self.normal_round:get_round_number()
+        if n == self.normal_round:get_max_round_number() then
+            self.next_round = self.final_round
         else
-            self.current_round = Normal_round
+            self.next_round = self.normal_round
         end
     else
         log.error('未知的回合触发回合调度算法！请检查！')
         return
     end
 
-    --xxx
-    --运算运算
-    --xxx
+    self:switch_special_round()
 
     self:create()
 end
 
 function mt:init_default_round(  )
-    self.current_round = Normal_round
+    self.next_round = self.normal_round
     self:create()
 end
 
 --调度目标回合开始创建
 function mt:create(  )
+    self.round_run_times = self.round_run_times + 1
+    self.current_round = self.next_round
     self.current_round:create()
+end
+
+
+--切换特殊回合
+function mt:switch_special_round( is_force )
+    if not self.special_round then
+        return
+    end
+    if self.next_round ~= self.final_round then
+        local rate = math.random(1, 100)
+        if is_force or self.special_round_rate <= rate and self.round_run_times % 2 == 0 then
+            self.run_special_round()
+            self.special_round_rate = self.special_round_base_rate
+        else
+            self.special_round_rate = self.special_round_rate + self.special_round_base_rate
+        end
+    end
+end
+
+
+--开启特殊回合（非最终回合）
+function mt:run_special_round(  )
+    self.special_round:create()
 end
 
 --创建战斗区域视野
@@ -72,12 +91,48 @@ end
 --目标回合已经结束，进行调度，准备下一回合
 --  已经结束的回合
 function mt:finish( finish_round )
-    switch_round(self, finish_round)
+    self:switch_round(self, finish_round)
 end
 
 function mt:init(  )
+    local Normal_round
+    local Boss_round
+    local Special_round
+    local Final_round = require 'rule.round.special_round.final_round'
+    local game_degree = Game_degree:get_game_degree()
+    local game_model = Game_degree:get_game_degree()
+
+    local lv = game_degree:get_game_degree_level()
+    if lv == 1 then
+        self.normal_round_switch_interval = 5
+    else
+        self.normal_round_switch_interval = 6
+        Special_round = require 'rule.round.special_round.special_round'
+        if lv == 2 then
+            self.special_round_base_rate = 5
+        else
+            self.special_round_base_rate = 20
+        end
+        self.special_round_rate = self.special_round_base_rate
+    end
+
+    if game_model:get_game_model_name() == '生存模式' then
+        Normal_round = require 'rule.round.normal_round.normal_round_exist'
+        Boss_round = require 'rule.round.boss_round.boss_round_exist'
+    else
+        Normal_round = require 'rule.round.normal_round.normal_round_guard'
+        Boss_round = require 'rule.round.boss_round.boss_round_exist_guard'
+    end
     Normal_round:init()
     Boss_round:init()
+
+    self.normal_round = Normal_round
+    self.boss_round = Boss_round
+    self.special_round = Special_round
+    if self.special_round then
+        self.special_round:init()
+    end
+
     self.round_finish_trg = ac.game:event '回合-结束-上交权限'(function ( trg, round )
         print('回合上交权限', round.type)
         self:finish(round)
@@ -86,14 +141,5 @@ function mt:init(  )
     self:create_battle_fog()
 end
 
-ac.game:event '游戏-开始回合逻辑'(function ()
-    --Round_core:init()
-    -- local round = require 'rule.round.normal_round.normal_round_guard'
-    -- round:init()
-    -- local round = require 'rule.round.boss_round.boss_round_guard'
-    local round = require 'rule.round.normal_round.normal_round_exist'
-    round:init()
-    round:create()
-end)
 
 return Round_core
