2bec613c5ab3e467488ff1c30de6d3392792af54
zhongying1995 <1424249327@qq.com>
2018-11-29 01:09:16 +0800

    修复单位不能捡起套装魂珠项链的错误

--- scripts/rule/pick_item.lua
+++ scripts/rule/pick_item.lua
@@ -1,29 +1,31 @@
 
 ac.game:event '单位-获得物品'(function (trg, unit, it)
     local is_drop
+    local fail_msg
     if it:is_weapon() then
         if not unit:is_enable_hold_weapon(it) then
             is_drop = true
-            local msg = ('你不能捡起非|cffff0000%s|r类型的武器!'):format(unit:get_weapon_type())
-            unit:get_owner():send_msg(msg, 2)
+            fail_msg = ('你不能捡起非|cffff0000%s|r类型的武器!'):format(unit:get_weapon_type())
         elseif unit:is_has_weapon(it) then
             is_drop = true
-            local msg = ('你只能拥有一件|cffff0000%s|r类型的武器!'):format(unit:get_weapon_type())
-            unit:get_owner():send_msg(msg, 2)
+            fail_msg = ('你只能拥有一件|cffff0000%s|r类型的武器!'):format(unit:get_weapon_type())
         end
         
     elseif it:is_necklace() then
-        if unit:is_has_necklace() then
+        if unit:is_has_necklace(it) then
+            fail_msg = ('你只能拥有一件|cffff0000%s|r!'):format('项链')
             is_drop = true
         end
         
     elseif it:is_soul_bead() then
-        if unit:is_has_soul_bead() then
+        if unit:is_has_soul_bead(it) then
+            fail_msg = ('你只能拥有一件|cffff0000%s|r!'):format('魂珠')
             is_drop = true
         end
         
     elseif it:is_corselet() then
-        if unit:is_has_corselet() then
+        if unit:is_has_corselet(it) then
+            fail_msg = ('你只能拥有一件|cffff0000%s|r!'):format('套装')
             is_drop = true
         end
         
@@ -31,6 +33,7 @@ ac.game:event '单位-获得物品'(function (trg, unit, it)
     
     if is_drop then
         unit:drop_item_down(it)
+        unit:get_owner():send_msg(fail_msg, 2)
     end
     
 end)
