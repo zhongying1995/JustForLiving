36cbf1c94ba4d4f6ede3c476b202d02998e33ffb
zhongying1995 <1424249327@qq.com>
2018-12-05 01:30:03 +0800

    第一版的种子

---
+++ scripts/rule/seed/_init.lua
@@ -0,0 +1,1 @@
+require 'rule.seed.seed_types._init'
---
+++ scripts/rule/seed/grow.lua
@@ -0,0 +1,80 @@
+local mt = ac.skill['生长']{
+    grow_size = 1.5,
+    grow_duration = 30,
+    fruit = '',
+}
+
+function mt:on_add(  )
+    local unit = self.owner
+    local duration = unit.grow_duration or self.grow_duration
+    local size = unit:get_size()
+    local max_size = unit.grow_size or self.grow_size
+
+    unit:add_buff('生长'){
+        skill = self,
+        duration = math.floor(duration),
+        size = size,
+        grow_size = max_size,
+    }
+end
+
+function mt:on_remove(  )
+    
+end
+
+
+
+local buff_mt = ac.buff['生长']{
+    model = [[Abilities\Spells\Other\StrongDrink\BrewmasterTarget.mdl]],
+    finish_model = [[Abilities\Spells\NightElf\FaerieFire\FaerieFireTarget.mdl]],
+}
+
+function buff_mt:on_add(  )
+    local unit = self.target
+    unit:add_effect(self.model):remove()
+    unit:transform_dummy()
+    local size = self.size / 2
+    unit:set_size(size)
+    local interval_size = (self.grow_size - size) / self.duration
+    self.grow_timer = ac.timer(1000, self.duration, function (  )
+        unit:add_size(interval_size)
+    end)
+    function self.grow_timer.on_remove(  )
+        unit:add_effect(self.finish_model):remove()
+        unit:transform_nondummy()
+        unit:add_buff('果实'){
+            duration = self.duration,
+            skill = self.skill,
+        }
+        self:remove()
+    end
+end
+
+function buff_mt:on_remove(  )
+    if self.grow_timer then
+        self.grow_timer:remove()
+    end
+end
+
+
+local buff_mt_1 = ac.buff['果实']{
+    radius = 256,
+}
+
+function buff_mt_1:on_add(  )
+    local unit = self.target
+    self.trg = unit:event '单位-被指向'(function ( trg, u, order, who, order_id )
+        if order == 'smart' and (who:get_point() * u:get_point()) < self.radius then
+            local item = unit:get_point():add_item(unit.fruit)
+            item:set_player(unit:get_owner())
+            unit:killed(u)
+            self:remove()
+        end
+    end)
+end
+
+function buff_mt_1:on_remove( )
+    if self.trg then
+        self.trg:remove()
+    end
+end
